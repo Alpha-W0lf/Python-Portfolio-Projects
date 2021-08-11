ffe671f92f2b5f3dcd4b951712fdf575951f77dc
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 14:30:06 -0500

    Update ai.py

--- projects-master/projects-master/my_sample_madlibs/ai.py
+++ projects-master/projects-master/my_sample_madlibs/ai.py
@@ -1,6 +1,12 @@
 def madlib():
+    adjective1 = input("Adjective: ")
+    noun1 = input("Plural noun: ")
+    noun2 = input("Noun: ")
+    verb1 = input("Verb: ")
+    noun3 = input("Noun: ")
 
-
-    madlib = f""
+    madlib = f"The possibilities of technology are {adjective1}. Humans have progressed from simple {noun1} to " \
+             f"masters of our {noun2}. Through human creativity and intelligence, we are able to {verb1} abundance. " \
+             f"The most exciting upcoming technology is {noun3}."
 
 print(madlib)
