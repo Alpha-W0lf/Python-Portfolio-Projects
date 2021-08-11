ae3d76115a63cef8b18aa699f602038f736c5d7b
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 14:54:53 -0500

    Update blockchain.py

---
+++ projects-master/projects-master/my_sample_madlibs/blockchain.py
@@ -0,0 +1,16 @@
+def madlib():
+    adjective1 = input("Adjective: ")
+    adjective2 = input("Adjective: ")
+    verb1 = input("Verb: ")
+    noun1 = input("Plural noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    verb2 = input("Verb: ")
+
+    madlib = f"In a world of {adjective1} central banks and {adjective2} fiat money, there is hope on the horizon. " \
+             f"Bitcoin offers independence. It offers choice to those who are paying attention. Governments are " \
+             f"{verb1} money faster than ever in history. Many {noun1} are going up in price because of an inflation " \
+             f"of the money {noun2}. Don't be a {noun3} and let your savings {verb2} into nothing. Invest in bitcoin " \
+             f"and other cryptocurrencies before it is too late!"
+
+print(madlib)
