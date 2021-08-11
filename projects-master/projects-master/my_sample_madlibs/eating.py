3da25d1eb2808ff3a4999ff0c845e63a83adf188
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 14:58:58 -0500

    Update eating.py

--- projects-master/projects-master/my_sample_madlibs/eating.py
+++ projects-master/projects-master/my_sample_madlibs/eating.py
@@ -1,6 +1,10 @@
 def madlib():
+    noun1 = input("Noun: ")
+    verb1 = input("Verb: ")
+    adjective1 = input("Adjective: ")
 
 
-    madlib = f""
+    madlib = f"Eating is like {noun1}. The more you eat, the more you {verb1}. Everyone has to eat and everyone loves" \
+             f" {adjective1} food. "
 
 print(madlib)
