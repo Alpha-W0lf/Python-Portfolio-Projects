2040da56ba8c278a4b5fcb9f8b98f25fe6edb203
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 20:02:34 -0500

    Update eating.py

--- projects-master/projects-master/my_sample_madlibs/eating.py
+++ projects-master/projects-master/my_sample_madlibs/eating.py
@@ -2,9 +2,13 @@ def madlib():
     noun1 = input("Noun: ")
     verb1 = input("Verb: ")
     adjective1 = input("Adjective: ")
-
+    city1 = input("City: ")
+    adjective2 = input("Adjective: ")
+    noun2 = input("Plural noun: ")
+    verb2 = input("Verb: ")
+    noun3 = input("Plural noun: ")
 
     madlib = f"Eating is like {noun1}. The more you eat, the more you {verb1}. Everyone has to eat and everyone loves" \
-             f" {adjective1} food. "
+             f" {adjective1} food. {city1} has a lot of {adjective2} {noun2}. I hope your next meal {verb2} with lots of great {noun3}!"
 
 print(madlib)
