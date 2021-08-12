bc6d0af5fd980710916fc42e5aaf765af0247845
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-12 10:56:29 -0500

    Update eating.py

--- projects-master/projects-master/my_sample_madlibs/eating.py
+++ projects-master/projects-master/my_sample_madlibs/eating.py
@@ -1,14 +1,21 @@
 def madlib():
-    noun1 = input("Noun: ")
     verb1 = input("Verb: ")
     adjective1 = input("Adjective: ")
-    city1 = input("City: ")
     adjective2 = input("Adjective: ")
-    noun2 = input("Plural noun: ")
+    city1 = input("City: ")
+    number1 = input("Number: ")
+    name1 = input("Name: ")
+    food1 = input("Food: ")
+    restaurant1 = input("Restaurant: ")
+    name2 = input("Name: ")
+    food2 = input("Food: ")
     verb2 = input("Verb: ")
-    noun3 = input("Plural noun: ")
-
-    madlib = f"Eating is like {noun1}. The more you eat, the more you {verb1}. Everyone has to eat and everyone loves" \
-             f" {adjective1} food. {city1} has a lot of {adjective2} {noun2}. I hope your next meal {verb2} with lots of great {noun3}!"
+    noun1 = input("Noun: ")
+    
+    madlib = f"I like to {verb1} lunch with my coworkers. These meals are usually {adjective1} and {adjective2} " \
+             f"especially since we live and work in {city1}. Every now and then, we come up with a product idea " \
+             f"worth ${number1}! My team mate {name1} really likes {food1}, so we eat at {restaurant1} pretty often." \
+             f" {name2} prefers {food2}, but nobody else really {verb2} it so they don't get their way very often. " \
+             f"Anyway, it's getting close to {noun1} time so let's catch up again later!"
 
 print(madlib)
