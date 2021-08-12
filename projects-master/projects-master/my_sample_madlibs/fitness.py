947987e5de6073ab7bcb226cc2b452b6e021e4f2
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-12 11:07:33 -0500

    Update fitness.py

--- projects-master/projects-master/my_sample_madlibs/fitness.py
+++ projects-master/projects-master/my_sample_madlibs/fitness.py
@@ -1,6 +1,18 @@
 def madlib():
+    name1 = ("Name: ")
+    activity1 = ("Fitness Activity: ")
+    location1 = ("Location: ")
+    name2 = ("Name: ")
+    activity2 = ("Fitness Activity: ")
+    adverb1 = ("Adverb: ")
+    verb1 = ("Verb: ")
+    verb2 = ("Verb: ")
+    noun1 = ("Noun: ")
+    adjective1 = ("Adjective: ")
 
-
-    madlib = f""
+    madlib = f"{name1} is a guy who really loves to {activity1}. Whenever he has time off, he can be found at the " \
+             f"local {location1} with {name2} who loves to {activity2}. Since both of them take fitness {adverb1}, " \
+             f"they get along great. Most people can {verb1} something they {verb2} to stay fit. With a little " \
+             f"{noun1}, fitness can be really {adjective1}!"
 
 print(madlib)
